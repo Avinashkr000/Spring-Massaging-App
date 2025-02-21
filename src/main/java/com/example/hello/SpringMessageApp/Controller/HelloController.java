885923bb5b68975ad8b52b206f78b2771db9d156
Avinashkr000 <akum97543@gmail.com>
2025-02-21 20:59:44 +0530

    [Avinash kumar] UC2 Solved

--- src/main/java/com/example/hello/SpringMessageApp/Controller/HelloController.java
+++ src/main/java/com/example/hello/SpringMessageApp/Controller/HelloController.java
@@ -1,15 +1,13 @@
 package com.example.hello.SpringMessageApp.Controller;
-
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
 public class HelloController {
 
-    @GetMapping
-    public String sayHello() {
-        return "Hello from BridgeLabz!";
+    @GetMapping("/hello/query")
+    public String sayHello(@RequestParam(defaultValue = "User") String name) {
+        return "Hello " + name + " from BridgeLabz!";
     }
 }
