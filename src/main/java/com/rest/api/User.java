d5c1e16cb4995eb404ee6a7d1842a5ccfe623aa8
Avinashkr000 <akum97543@gmail.com>
2025-02-22 09:57:04 +0530

    [Avinash kumar] UC5 Solved

--- src/main/java/com/rest/api/User.java
+++ src/main/java/com/rest/api/User.java
@@ -3,12 +3,11 @@ package com.rest.api;
 public class User {
     String firstName;
     String lastName;
-
     public String getFirstName() {
         return firstName;
     }
-
     public String getLastName() {
         return lastName;
     }
+
 }
